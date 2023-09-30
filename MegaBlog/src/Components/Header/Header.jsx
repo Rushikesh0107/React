e8ade9f6b960626215388be418a7ec59a446fcb3
Rushikesh Dadas <rushikeshdadas03@gmail.com>
2023-09-30 16:06:44 +0530

    lil issues fixed

--- MegaBlog/src/Components/Header/Header.jsx
+++ MegaBlog/src/Components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import {Container, Logo, Logout} from '../index'
+import {Container, Logo, LogoutBtn} from '../index'
 import {Link} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import LogoutBtn from '../index'
 
 function Header() {
 
