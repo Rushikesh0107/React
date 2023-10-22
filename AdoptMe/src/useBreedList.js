bfd2dc867da04c93af985d253c71661744839839
Rushikesh Dadas <rushikeshdadas03@gmail.com>
2023-10-22 17:19:07 +0530

    detach

--- AdoptMe/src/useBreedList.js
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import fetchBreedList from "./fetchBreedList";
-
-export default function useBreedList(animal) {
-    const result = useQuery(['breedList', animal], fetchBreedList);
-    return [result?.data?.breeds ?? [],result.status]
-}
